82f3afb0f96a26f61a68e4d91a7fe274b6aae0e0
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-05 14:10:49 -0400

    added a title field for Launch.js for now, will add more field later when we figure out what we want on our launch page

--- models/Launch.js
+++ models/Launch.js
@@ -5,5 +5,31 @@ const sequelize = require("../config/connection");
 //Initialize Favorite model by extending off Sequelize's Model Class
 class Launch extends Model {};
 
+//Set up fields and rules for Launch model 
+Launch.init ( 
+    {
+        //Id field for Launch model
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        //title fielf for Launch model 
+        title: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+    },
+    {
+        //Rules for Launch model 
+        sequelize,  
+        freezeTableName: true, 
+        underscored: true, 
+        modelName: "Favorite"
+    }
+);
+
 //Export Launch model 
 module.exports = Launch;
