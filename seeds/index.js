8632a0d205a960a0192ef6ea9107dbbd12043e8c
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-05 14:33:42 -0400

    added the files in the seeds/index.js that will seedAll the data created into the database

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+//Import required seed model and packages
+const seedUsers = require("./userSeed");
+const seedFavorites  = require("./favorite-seed");
+const seedProfiles = require("./profile-seed");
+const seedLaunches = require("./launch-seed");
+const sequelize = require("../config/connection");
+
+//Seed all the model to the database
+const seedAll = async () => {
+
+    //Create the table, dropping it first if it already existed
+    await sequelize.sync({ force: true });
+
+    //Seed the User model
+    await seedUsers();
+    console.log("\n---------- Users Seeded ----------\n");
+
+    //Seed the Favorite model 
+    await seedFavorites();
+    console.log("\n---------- Favorites Seeded ----------\n");
+
+    //Seed the Profile model 
+    await seedProfiles();
+    console.log("\n---------- Profile Seeded ----------\n");
+
+    //Seed the Profile model 
+    await seedLaunches();
+    console.log("\n---------- Launch Seeded ----------\n");
+
+    //End process when all model is seeded 
+    process.exit();
+};
+
+//Call the function
+seedAll();
