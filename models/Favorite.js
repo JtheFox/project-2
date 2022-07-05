915ebbc1ddebac4d3789813705e160cab7231c9a
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-05 13:39:24 -0400

    added a id field and set rules for Favorite.js file in models

--- models/Favorite.js
+++ models/Favorite.js
@@ -5,5 +5,26 @@ const sequelize = require("../config/connection");
 //Initialize Favorite model by extending off Sequelize's Model Class
 class Favorite extends Model {};
 
+//Set up fields and rules for Favorite model 
+Favorite.init (
+    {
+        //ID field for user 
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            autoIncrement: true, 
+            primaryKey: true
+        }
+    },
+    {
+        //Rules for Favorite model 
+        sequelize, 
+        timestamps: false, 
+        freezeTableName: true, 
+        underscored: true, 
+        modelName: "Favorite"
+    }
+);
+
 //Export Favorite module
 module.exports = Favorite;
