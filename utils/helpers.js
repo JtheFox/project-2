154d06fe9d5c3c9112d3d1b3c92178963d293f1b
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-05 14:35:51 -0400

    add spacex api data parser

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,26 @@
-//
+module.exports = {
+    parseLaunchData: (launch) => {
+        return {
+            id: launch.id,
+            name: launch.name,
+            success: launch.success,
+            date: launch.date_unix,
+            flight_number: launch.flight_number,
+            icon: launch.links.patch.large,
+            forum: launch.links.reddit.launch,
+            webcast: launch.links.webcast,
+            wiki: launch.links.wikipedia,
+        }
+    },
+    parseRocketData: (rocket) => {
+        return {
+            id: rocket.id,
+            name: rocket.name,
+            first_flight: rocket.first_flight,
+            image: rocket.flickr_images[0],
+            wiki: rocket.wikipedia,
+            description: rocket.description,
+            launch_cost: rocket.cost_per_launch
+        }
+    }
+}
