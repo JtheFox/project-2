2236b3bd782c9e3c42886c1ceb12a33a83696ff0
raydover <105178034+raydover@users.noreply.github.com>
2022-07-06 08:06:47 -0400

    create launch routes, GET, CREATE, UPDATE, DELETE, GET by ID

---
+++ controllers/api/profile-route.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { Profile } = require("../../models");
+
+// GET all Profiles
+
+
+// Create a new Profile
+
+
+// UPDATE a Profile
+
+
+// GET one Profile
+
+
+//   Delete a Profile
+
+
+
+module.exports = router;
