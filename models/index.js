43c9d1e02d8bba8437305a483ae392833ffdaaff
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-06 12:23:32 -0400

    fix error in the const user = require(./User), forgot the dot in frot of /User, added a rocket Model in model/Rocket.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 //Import models
-const User = require('/User');
+const User = require('./User');
 const Favorite = require('./Favorite');
 const Profile = require('./Profile');
 const Launch = require('./Launch');
