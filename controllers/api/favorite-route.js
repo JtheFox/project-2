b0eff5acbbb3688a5d51f5ba48113564f9520059
raydover <105178034+raydover@users.noreply.github.com>
2022-07-06 10:54:29 -0400

    create favorite-route.js file

--- controllers/api/favorite-route.js
+++ controllers/api/favorite-route.js
@@ -2,19 +2,87 @@ const router = require("express").Router();
 const { Favorite } = require("../../models");
 
 // GET all Favorites
-
+router.get("/", async (req, res) => {
+    try {
+      const favoriteData = await Favorite.findAll();
+      res.status(200).json(favoriteData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // Create a new Favorite
-
+router.post("/", async (req, res) => {
+    try {
+      const newFavorite = await Favorite.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newFavorite);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 // UPDATE a Favorite
-
+router.put('/:id', async (req, res) => {
+    try {
+      const favoriteData = await Favorite.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!favoriteData[0]) {
+        res.status(404).json({ message: 'No Favorite with this id!' });
+        return;
+      }
+      res.status(200).json(favoriteData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // GET one Favorite
-
+router.get('/:id', async (req, res) => {
+    try {
+      const favoriteData = await Favorite.findByPk(req.params.id);
+      if (!favoriteData) {
+        res.status(404).json({ message: 'No Favorite with this id!' });
+        return;
+      }
+      res.status(200).json(favoriteData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 //   Delete a Favorite
-
-
+router.delete("/:id", async (req, res) => {
+    try {
+      const favoriteData = await Favorite.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!favoriteData) {
+        res.status(404).json({ message: "No Favorite found with this id!" });
+        return;
+      }
+  
+      res.status(200).json(favoriteData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
+
+
+
+
+
+
+
--- controllers/api/launch-route.js
+++ controllers/api/launch-route.js
@@ -11,38 +11,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Create a new launch
-router.post("/", async (req, res) => {
-  try {
-    const newLaunch = await Launch.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newLaunch);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// UPDATE a launch
-router.put('/:id', async (req, res) => {
-    try {
-      const launchData = await Launch.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!launchData[0]) {
-        res.status(404).json({ message: 'No Launch with this id!' });
-        return;
-      }
-      res.status(200).json(launchData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
  // GET one launch
  router.get('/:id', async (req, res) => {
     try {
@@ -56,26 +24,5 @@ router.put('/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
-
-//   Delete a Launch
-router.delete("/:id", async (req, res) => {
-  try {
-    const launchData = await Launch.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!launchData) {
-      res.status(404).json({ message: "No Launch found with this id!" });
-      return;
-    }
-
-    res.status(200).json(launchData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
