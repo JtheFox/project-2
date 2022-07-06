e2a8a8368b21c0aa9cddf573baa2d4b93b32ad51
GrahamP98 <g.purnell15@gmail.com>
2022-07-06 18:02:55 -0400

    started on query route for the launches but still need to figure it out.calling it for the day thoough.

--- controllers/api/launch-routes.js
+++ controllers/api/launch-routes.js
@@ -25,4 +25,18 @@ router.get("/", async (req, res) => {
     }
   });
 
+// // POST query launch
+//   router.post('/query/', function (req, res) {
+//     try {
+//       const launchData = await Launch.findByPk(req.params.id);
+//       if (!launchData) {
+//         res.status(404).json({ message: 'No Launch with this id!' });
+//         return;
+//       }
+//       res.status(200).json(launchData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
 module.exports = router;
