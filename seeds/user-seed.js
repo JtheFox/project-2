4fb6ba9a4e94e0eebc74c88d5b8df929a44c0242
raydover <105178034+raydover@users.noreply.github.com>
2022-07-07 12:24:30 -0400

    commit all recent changes

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -5,27 +5,33 @@ const { User } = require('../models');
 const userData = [
     {
         'username': 'mattSawyer', 
-        'password': 'mattSawyer123'
+        'password': 'mattSawyer123',
+        'launch_id': '5eb87cdfffd86e000604b331'
     },
     {
         'username': 'lincolnWales',
-        'password': 'lincolnWales123'
+        'password': 'lincolnWales123',
+        'launch_id': '5eb87cdeffd86e000604b330'
     },
     {
         'username': 'johnHeight',
-        'password': 'johnHeight123'
+        'password': 'johnHeight123',
+        'launch_id': '5eb87ce0ffd86e000604b332'
     },
     {
         'username': 'maryTate', 
-        'password': 'maryTate123'
+        'password': 'maryTate123',
+        'launch_id': '5eb87ce5ffd86e000604b339'
     },
     {
         'username': 'allisonRhode',
-        'password': 'allisonRhode123'
+        'password': 'allisonRhode123',
+        'launch_id': '5eb87ce1ffd86e000604b333'
     },
     {
         'username': 'susanLee',
-        'password': 'susanLee123'
+        'password': 'susanLee123',
+        'launch_id': '5eb87ce3ffd86e000604b336'
     }
 ];
 
