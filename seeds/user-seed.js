4cf1307708051f34181adeea2e8cf79b604a009c
raydover <105178034+raydover@users.noreply.github.com>
2022-07-07 11:39:43 -0400

    added launch ids, changed double quotes to single quotes

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -4,28 +4,28 @@ const { User } = require('../models');
 //Create a list of user 
 const userData = [
     {
-        "username": "mattSawyer", 
-        "password": "mattSawyer123"
+        'username': 'mattSawyer', 
+        'password': 'mattSawyer123'
     },
     {
-        "username": "lincolnWales",
-        "password": "lincolnWales123"
+        'username': 'lincolnWales',
+        'password': 'lincolnWales123'
     },
     {
-        "username": "johnHeight",
-        "password": "johnHeight123"
+        'username': 'johnHeight',
+        'password': 'johnHeight123'
     },
     {
-        "username": "maryTate", 
-        "password": "maryTate123"
+        'username': 'maryTate', 
+        'password': 'maryTate123'
     },
     {
-        "username": "allisonRhode",
-        "password": "allisonRhode123"
+        'username': 'allisonRhode',
+        'password': 'allisonRhode123'
     },
     {
-        "username": "susanLee",
-        "password": "susanLee123"
+        'username': 'susanLee',
+        'password': 'susanLee123'
     }
 ];
 
