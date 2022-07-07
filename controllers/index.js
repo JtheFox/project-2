ddd27c996e2684a195ffc8e9a2268cd1e6a91189
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-07 13:12:18 -0400

    created the routes in the main handlebars and import the search routes in the controller/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
+//Import required packages and routes
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const searchRoutes = require('./search-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;
