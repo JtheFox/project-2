0f24cd7a31f695653ae6ee9bc1bb3271bb8c24b7
raydover <105178034+raydover@users.noreply.github.com>
2022-07-08 08:41:30 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// Pulls in required dependencies
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+// Import express-handlebars
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
 const routes = require('./controllers');
+// Import the connection object - config directory - connection.js file
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
+// Gives express name app
 const app = express();
+// Port 3001 on machine - hosting service
 const PORT = process.env.PORT || 3001;
 
 // Set up sessions
@@ -29,15 +35,20 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
+// Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware - form submission JSON object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
 app.use(routes);
 
+// turn on connection to db and server
+// Force true to drop/recreate table(s) on every sync
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
