fd895a09e9787fcceea0fbe398bed9bf588520a4
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-07 21:04:55 -0400

    added the comment model, implement the fields and rules

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+//Import required packages
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//Initialize User model (table) by extending off Sequelize's Model Class
+class Comment extends Model {};
+
+//Set up fields and rules for Comment model 
+Comment.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            autoIncrement: true, 
+            primaryKey: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "User",
+                key: "id"
+            }
+        },
+        launch_id: {
+            type: DataTypes.CHAR(24),
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        }
+    }
+);
+
+//Export Comment model
+module.exports = Comment;
