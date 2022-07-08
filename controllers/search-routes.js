d7d3ff1936407058a1448c7e7f8888ebc8f20948
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-08 02:36:37 -0400

    minor fix

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
     try {
         const nextLaunch = await getNextLaunch();
         
-        res.status(200).json(search);
+        res.status(404).json();
     } catch (err) {
         res.status(500).json(err);
     }
